6fa75a3fe01493485896cd847b853c3b95392f91
Arnav Shah <arnav.shah.2k10@gmail.com>
2026-02-02 11:55:10 -0800

    chore: Fix when the .env was not working

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.mn8mop0lphs"
+    "revision": "0.gnbvp9a01i"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
